91315a92ffff0ce6cfbf9cf8db75fbb9c740826d
Maurice Fallon <maurice.fallon@ed.ac.uk>
2015-11-25 19:45:22 +0000

    bigman changes

--- src/python/director/roboturdf.py
+++ src/python/director/roboturdf.py
@@ -296,6 +296,7 @@ def setupPackagePaths():
         'software/models/atlas_v5',
         'software/models/valkyrie',
         'software/models/val_description',
+        'software/models/bigman_urdf',
         'software/models/lwr_defs',
         'software/models/mit_gazebo_models/mit_robot',
         'software/models/mit_gazebo_models/V1',
