3037ec5edd119b89219e2dcd4a9232999511da54
Maurice Fallon <mauricefallon@gmail.com>
2016-02-23 13:51:13 +0000

    hacked out point cloud stuff for now

--- src/python/tests/testDrawRobotLog.py
+++ src/python/tests/testDrawRobotLog.py
@@ -5,8 +5,8 @@ from director import planplayback
 from director import playbackpanel
 from director import robotplanlistener
 from director import robotviewbehaviors
-from director import pointcloudlcm
-from director import cameraview
+#from director import pointcloudlcm
+#from director import cameraview
 from director import lcmUtils
 from PythonQt import QtCore, QtGui
 import drc as lcmdrc
@@ -35,20 +35,20 @@ playbackPanel = playbackpanel.PlaybackPanel(planPlayback, playbackRobotModel, pl
 manipPlanner.connectPlanReceived(playbackPanel.setPlan)
 
 # initialize pointcloud lcm
-pointcloudlcm.init(view)
+#pointcloudlcm.init(view)
 
 # initialize camera view
-cameraChannel = 'CAMERA_LEFT'
-imageManager = cameraview.ImageManager()
-imageManager.queue.addCameraStream(cameraChannel)
-imageManager.addImage(cameraChannel)
-cameraView = cameraview.CameraImageView(imageManager, cameraChannel, view=app.createView(useGrid=False))
+#cameraChannel = 'CAMERA_LEFT'
+#imageManager = cameraview.ImageManager()
+#imageManager.queue.addCameraStream(cameraChannel)
+#imageManager.addImage(cameraChannel)
+#cameraView = cameraview.CameraImageView(imageManager, cameraChannel, view=app.createView(useGrid=False))
 
 # show widgets with a grid layout
 w = QtGui.QWidget()
 l = QtGui.QGridLayout(w)
 l.addWidget(view, 0, 0)
-l.addWidget(cameraView.view, 0, 1)
+#l.addWidget(cameraView.view, 0, 1)
 l.addWidget(playbackPanel.widget, 1, 0, 1, 2) # row, column, row span, column span
 l.setContentsMargins(0, 0, 0, 0)
 w.resize(1024, 600)
